31ed0bbe51b26954146edd32bef7c4ac080935c4
Abhay P. Nambiar <nambiar.abhay2000@gmail.com>
2026-05-22 18:15:32 +0530

    chore: set up file stucture and pre commit checks

--- src/sesame/db/db.py
+++ src/sesame/db/db.py
@@ -1,23 +1,27 @@
-import sqlite3
 import os
-from dotenv import load_dotenv
+import sqlite3
 import subprocess
 import sys
+
+from dotenv import load_dotenv
 
 load_dotenv()
 db_path = os.getenv("DATABASE")
 print("Database path: ", db_path)
+
+
 def init_db():
     if os.path.exists(db_path):
         conn = sqlite3.connect(db_path)
         c = conn.cursor()
-        c.execute('''
-        SELECT * FROM PASSWORDS''')
+        c.execute("""
+        SELECT * FROM PASSWORDS""")
         rows = c.fetchall()
         res = [(row[0], row[1], row[3]) for row in rows]
         # conn.commit()
         conn.close()
         return res
+
 
 def copy_to_clipboard(text):
     command = ""
@@ -33,15 +37,19 @@ def copy_to_clipboard(text):
     except Exception as e:
         print("Error copying to clipboard: ", e)
 
+
 def fetch_password_by_id(pwd_id):
     if os.path.exists(db_path):
         conn = sqlite3.connect(db_path)
         c = conn.cursor()
-        c.execute('''
-        SELECT * FROM PASSWORDS WHERE id=?''', (pwd_id,))
+        c.execute(
+            """
+        SELECT * FROM PASSWORDS WHERE id=?""",
+            (pwd_id,),
+        )
         row = c.fetchone()
         conn.close()
         if row:
             copy_to_clipboard(str(row[2]))
             return True
-    return False 
+    return False
